a38a377cac2fa0e957edde18d8eb850b0b1683f9
rib <poleyrobert@gmail.com>
2026-09-18 20:31:07 -0400

    Made Message Serializable

--- src/Message.java
+++ src/Message.java
@@ -1,4 +1,7 @@
-public class Message {
+import java.io.Serializable;
+
+public class Message implements Serializable {
+    private static final long serialVersionUID = 1L;
     String userName;
     String message;
 
